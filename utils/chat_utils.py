95b8d4ac63ced126064ae6e8eb184393dfb4146e
hdnh2006 <contact@henrynavarro.org>
2024-02-21 16:14:12 +0100

    system role template modified

--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -6,7 +6,45 @@ Created on Mon Dec  4 15:54:10 2023
 @author: henry
 """
 
-import torch
+
+DEFAULT_SYSTEM_PROMPT = "You are a helpful bot created by the AI team of Open Sistemas, an innovative company in the are of AI & Data. Your answers are clear and concise and you must respond in the same language the user asked you."
+
+LICENSE = """
+<p/>
+
+---
+As a derivate work of Code Llama by Meta,
+this demo is governed by the original [license](https://huggingface.co/spaces/huggingface-projects/codellama-2-13b-chat/blob/main/LICENSE.txt) and [acceptable use policy](https://huggingface.co/spaces/huggingface-projects/codellama-2-13b-chat/blob/main/USE_POLICY.md).
+"""
+
+def clear_and_save_textbox(message: str) -> tuple[str, str]:
+    return '', message
+
+
+
+def display_input(message: str,
+                  history: list[tuple[str, str]]) -> list[tuple[str, str]]:
+    history.append((message, ''))
+    return history
+
+
+def delete_prev_fn(
+        history: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], str]:
+    try:
+        message, _ = history.pop()
+    except IndexError:
+        message = ''
+    return history, message or ''
+
+
+def support_system(tokenizer):
+    try:
+        # Attempt to use a system message with the tokenizer
+        test_message = [{"role": "system", "content": "Test system support"}]
+        tokenizer.apply_chat_template(test_message, tokenize=False)
+        return True  # System messages are supported
+    except:
+        return False  # System messages are not supported
 
 # Formatting function for message and history
 def format_message(tokenizer, message: str, history: list, memory_limit: int = 4) -> str:
@@ -23,15 +61,16 @@ def format_message(tokenizer, message: str, history: list, memory_limit: int = 4
         str: Formatted message string
     """
     
-    formatted_message = [
-        {"role": "system", "content": "You are a helpful bot created by the AI team of Open Sistemas, an innovative company in the are of AI & Data. Your answers are clear and concise and you must respond in the same language the user asked you."}
-    ]
-    
-    # Mistral doesn't support system role
-    # formatted_message = [
-    #     {"role": "user", "content": "Who did create you?"},
-    #     {"role": "assistant", "content": "I was created by the AI team of Open Sistemas, an innovative company in the are of AI & Data."}
-    # ]
+    # check if template supports system role
+    if support_system(tokenizer):
+        formatted_message = [
+            {"role": "system", "content": DEFAULT_SYSTEM_PROMPT}
+        ]
+    else:
+        formatted_message = [
+            {"role": "user", "content": "Who did create you?"},
+            {"role": "assistant", "content": "I was created by the AI team of Open Sistemas, an innovative company in the are of AI & Data."}
+        ]
     
     # always keep len(history) <= memory_limit
     if len(history) > memory_limit:
